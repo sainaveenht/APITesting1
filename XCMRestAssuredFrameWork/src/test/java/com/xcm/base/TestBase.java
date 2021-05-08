e07054093480587f3ab8f319affef2c97d4aead8
Admin <Admin@DESKTOP-FPPI0PB>
2021-05-08 22:44:54 +0530

    First commit - Adding API Project (Employe Demo)

---
+++ XCMRestAssuredFrameWork/src/test/java/com/xcm/base/TestBase.java
@@ -0,0 +1,33 @@
+package com.xcm.base;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.annotations.BeforeClass;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TestBase {
+
+	
+	public static Response response;
+	public static RequestSpecification httpRequest;
+	public String empID="58512"; //Hard coded - Input for Get details of Single Employee & update employee
+	
+	public Logger logger;
+	
+	
+	@BeforeClass
+	public void setup()
+	{
+		logger = Logger.getLogger("XcmRestAPI");
+		PropertyConfigurator.configure("Log4j.properties");
+		logger.setLevel(Level.DEBUG);
+		
+	}
+	
+	
+	
+	
+}
